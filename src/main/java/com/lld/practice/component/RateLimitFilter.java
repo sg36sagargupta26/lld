2c29f468bd1072e520b35a31d970a4f771682531
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2026-03-19 19:35:24 +0530

    changed from handler interceptor to filter

---
+++ src/main/java/com/lld/practice/component/RateLimitFilter.java
@@ -0,0 +1,36 @@
+package com.lld.practice.component;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@Component
+public class RateLimitFilter implements Filter {
+    private final Map<String, Integer> requestCounts = new ConcurrentHashMap<>();
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String userId = request.getHeader("user-id");
+        if (request.getRequestURI().contains("hello") && userId != null) {
+            int count = requestCounts.merge(userId, 1, Integer::sum);
+            if (count > 10) {
+                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                response.getWriter().write("Rate limit exceeded");
+                return; // Stop the request
+            }
+        }
+        chain.doFilter(req, res); // Continue to the RestController
+    }
+}
